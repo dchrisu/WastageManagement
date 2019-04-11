bea67e9906e4855420aca134ce46bf0ed5e633e4
dchrisu <dchrisu@yahoo.com>
2019-04-11 15:29:22 -0500

    view wastage changes

--- src/Pages/ViewWastage/ViewWastage.js
+++ src/Pages/ViewWastage/ViewWastage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Chart } from "react-google-charts";
 import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';    
+import classNames from 'classnames';
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 
 const styles = theme => ({
   appBar: {
@@ -18,19 +22,31 @@ class ViewWastage extends React.Component {
         zipcode: "",
         recycle: 12,
         waste: 1,
-        data: [{recycle: 4, waste: 2},{recycle: 4, waste: 2}],
+        data: [],
     };
 
     componentDidMount(){
         var today = new Date();
         this.state.data = today.getDate();
-        this.state.current = 1;
     }
 
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
     getWastage(){
-        fetch('http://localhost:8081/')
+        fetch('http://localhost:8081/getDetails', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+             },
+            body: JSON.stringify({
+                date: this.state.date,
+                zipcode: this.state.zipcode,
+            })
+        })
         .then(res => res.json())
-        .then(result => this.setState({data: result.data}))
+        .then(result => this.setState({data: result.data, recycle: result.data.recycle, waste: result.data.waste}))
         .catch(err => console.log(err))
     };
     
@@ -39,7 +55,33 @@ class ViewWastage extends React.Component {
         return(
             <div class = "container">
                 <h1 style = {{textAlign: 'center'}}>View Today's Wastage</h1>
+                <div class = "container" style = {{padding: '20px',textAlign: 'center'}}>
+                    <Paper>
+                        <TextField
+                        id="standard-name"
+                        label="Date"
+                        defaultValue={this.state.date}
+                        value={this.state.date}
+                        onChange={this.handleChange('date')}
+                        className={classNames(classes.textField, classes.dense)}
+                        margin="normal"
+                        />
+
+                        <TextField
+                        id="standard-name"
+                        label="ZIPcode"
+                        value={this.state.ZIPcode}
+                        onChange={this.handleChange('zipcode')}
+                        className={classNames(classes.textField, classes.dense)}
+                        margin="normal"
+                        />
+
+                        <Button variant="contained" className={classes.button} onClick = {() => this.getWastage()}>
+                            Update
+                        </Button>
+                    </Paper>
                         
+                </div>
                 <div className={"my-pretty-chart-container"} style = {{width: '70%', margin: '0 auto'}}>
                     <Chart
                     width={'500px'}
